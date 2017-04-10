06e18d3eae213f732c542b8ac838ff2d59082bb3
Robert Ball <robert.ball.1989@gmail.com>
2017-04-10 15:21:49 +0100

    Added programmatic cat-adding function.

--- public/app.js
+++ public/app.js
@@ -1,24 +1,50 @@
-var app = function(){
+var createCatUL = function(){
   var catUL = document.createElement('ul')
   catUL.classList.add('cat')
+  return catUL
+}
 
+var createCatName = function(name){
   var catName = document.createElement('li')
-  catName.innerText = 'Shere Khan'
+  catName.innerText = name
+  return catName
+}
 
+var createCatFood = function(food){
   var catFood = document.createElement('li')
-  catFood.innerText = 'Man'
+  catFood.innerText = food
+  return catFood
+}
 
+var createCatImg = function(url){
+  var catImg 
   var catImg = document.createElement('li')
-  catImg.innerHTML = '<img width ="500" src="https://upload.wikimedia.org/wikipedia/commons/thumb/5/54/Tigress_at_Jim_Corbett_National_Park.jpg/1200px-Tigress_at_Jim_Corbett_National_Park.jpg">'
+  imgString = '<img width="500" src=' + url + '>'
+  catImg.innerHTML = imgString
+  return catImg
+}
 
-  catUL.appendChild(catName)
-  catUL.appendChild(catFood)
-  catUL.appendChild(catImg)
+var appendElements = function(ul,name,food,img){
+  ul.appendChild(name)
+  ul.appendChild(food)
+  ul.appendChild(img)
 
   var cats = document.querySelector('#cats')
 
-  cats.appendChild(catUL)
+  cats.appendChild(ul)
+}
 
+var createCat = function(name,food,img){
+  var ul = createCatUL()
+  var name = createCatName(name)
+  var food = createCatFood(food)
+  var img = createCatImg(img)
+
+  appendElements(ul,name,food,img)
+}
+
+var app = function(){
+  createCat('Shere Khan', 'Human',"tiger.jpg")
 }
 
 window.onload = app;
